ad28ef179c811a4cdd2da949a6412dd9c0a1ed18
titus njiru <njirutitus@gmail.com>
2021-01-26 13:26:27 +0300

    Adding User class

--- main.cpp
+++ main.cpp
@@ -1,15 +1,5 @@
 #include <iostream>
 # include<vector>
-FILE *fp;
-struct User{
-	char name[200];
-	int gender;
-	char phone_number[15];
-	char id_no[20];
-	unsigned int is_staff;
-	unsigned int is_admin;
-	char password[255];
-};
 
 struct Book {
 	char title[200];
@@ -18,11 +8,11 @@ struct Book {
 	int copies;
 };
 
-User add_user();
 Book add_book();
 int main(int argc, char** argv) {
-	User user;
 	Book book;
+	User user;
+	char name[200];
 	int choice {0};
 	std::cout<<"\tCounty Library management system"<<std::endl;
 	std::cout<<"1. Add User\n";
@@ -31,8 +21,8 @@ int main(int argc, char** argv) {
 	std::cin>>choice;
 	switch(choice) {
 		case 1:
-			user = add_user();
-			std::cout<<user.name<<" Successfully added"<<std::endl;
+			user.add_user();
+			std::cout<<"User Successfully added"<<std::endl;
 			break;
 		case 2:
 			book = add_book();
@@ -44,59 +34,6 @@ int main(int argc, char** argv) {
 	}
 	
 	return 0;
-}
-
-User add_user(){
-	struct User user;
-	int choice {0};
-	std::cout<<"Enter name"<<std::endl;
-	std::cin.getline(user.name,200);
-	std::cout<<"Select gender"<<std::endl;
-	std::cout<<"1. Male"<<std::endl;
-	std::cout<<"2. Female"<<std::endl;
-	std::cout<<"Your choice: "<<std::endl;
-	std::cin>>choice;
-	do{
-		if(choice == 1){
-			user.gender = 1;
-		}
-		else if(choice == 2){
-			user.gender = 0;
-		}
-		else {
-			std::cout<<"Invalid choice. Try again";
-		}
-	}while(choice!=1 && choice!=2);
-	
-	std::cout<<"ID no: ";
-	std::cin>>user.id_no;
-	std::cout<<"Select User type"<<std::endl;
-	std::cout<<"1. Student"<<std::endl;
-	std::cout<<"2. Staff"<<std::endl;
-	std::cout<<"3. Admin"<<std::endl;
-	std::cout<<"Your choice: "<<std::endl;
-	std::cin>>choice;
-	do{
-		if(choice == 1){
-			user.is_admin = 0;
-			user.is_staff = 0;
-		}
-		else if(choice == 2){
-			user.is_admin = 0;
-			user.is_staff = 1;
-		}
-		else if(choice == 3){
-			user.is_admin = 1;
-			user.is_staff = 0;
-		}
-		else {
-			std::cout<<"Invalid choice. Try again";
-		}
-	}while(choice!=1 && choice!=2 && choice!=2);
-	std::cout<<"Enter Default password"<<std::endl;
-	std::cin>>user.password;
-	return user;
-	
 }
 
 Book add_book(){
@@ -112,6 +49,69 @@ Book add_book(){
 	std::cin>>book.copies;
 
 	return book;
+}
+
+class User{
+	char name[200];
+	int gender;
+	char phone_number[15];
+	char id_no[20];
+	unsigned int is_staff;
+	unsigned int is_admin;
+	char password[255];
+public:
+	void add_user();
+};
+
+void User::add_user(){
+	int choice {0};
+	std::cout<<"Enter name"<<std::endl;
+	std::cin.getline(name,200);
+	std::cout<<"Select gender"<<std::endl;
+	std::cout<<"1. Male"<<std::endl;
+	std::cout<<"2. Female"<<std::endl;
+	std::cout<<"Your choice: "<<std::endl;
+	std::cin>>choice;
+	do{
+		if(choice == 1){
+			gender = 1;
+		}
+		else if(choice == 2){
+			gender = 0;
+		}
+		else {
+			std::cout<<"Invalid choice. Try again";
+		}
+	}while(choice!=1 && choice!=2);
+	
+	std::cout<<"ID no: ";
+	std::cin>>id_no;
+	std::cout<<"Select User type"<<std::endl;
+	std::cout<<"1. Student"<<std::endl;
+	std::cout<<"2. Staff"<<std::endl;
+	std::cout<<"3. Admin"<<std::endl;
+	std::cout<<"Your choice: "<<std::endl;
+	std::cin>>choice;
+	do{
+		if(choice == 1){
+			is_admin = 0;
+			is_staff = 0;
+		}
+		else if(choice == 2){
+			is_admin = 0;
+			is_staff = 1;
+		}
+		else if(choice == 3){
+			is_admin = 1;
+			is_staff = 0;
+		}
+		else {
+			std::cout<<"Invalid choice. Try again";
+		}
+	}while(choice!=1 && choice!=2 && choice!=2);
+	std::cout<<"Enter Default password"<<std::endl;
+	std::cin>>password;
+	
 }
 
 
