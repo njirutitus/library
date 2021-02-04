91a233e6652d1dc788ff86c80ca660410145b881
titus njiru <njirutitus@gmail.com>
2021-02-05 01:39:42 +0300

    Scraping reading user inputs from keyboard

--- main.cpp
+++ main.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
-# include<vector>
+#include<vector>
+#include<string.h>
 class Book{
 	char title[200];
 	char author[200];
 	char ISBN[20];
 	int copies;
 public:
-	void add_book();
+	void add_book(char t[200],char a[200],char i[20], int c);
 };
 
 class User{
@@ -18,7 +19,7 @@ class User{
 	bool is_admin;
 	char password[255];
 public:
-	void add_user();
+	void add_user(char n[200],std::string g,char p[15],char id[20], bool stf, bool admn, char pass[255]);
 };
 
 class Calendar {
@@ -32,67 +33,21 @@ public:
 	int day_of_the_week(int year, int month, int day);
 };
 
-void Book::add_book(){
-	std::cout<<"\t Add a new book\n";
-	std::cout<<"Book title:";
-	std::cin.getline(title,200,'*');
-	std::cout<<"Book Author";
-	std::cin.getline(author,200,'*');
-	std::cout<<"ISBN: ";
-	std::cin>>ISBN;
-	std::cout<<"Copies: ";
-	std::cin>>copies;
+void Book::add_book(char t[200],char a[200],char i[20], int c){
+	strcpy(title,t);
+	strcpy(author,a);
+	strcpy(ISBN,i);
+	copies = c;
 
 }
 
-void User::add_user(){
-	int choice {0};
-	std::cout<<"Enter name"<<std::endl;
-	std::cin.getline(name,200);
-	std::cout<<"Select gender"<<std::endl;
-	std::cout<<"1. Male"<<std::endl;
-	std::cout<<"2. Female"<<std::endl;
-	std::cout<<"Your choice: "<<std::endl;
-	std::cin>>choice;
-	do{
-		if(choice == 1){
-			gender = "male";
-		}
-		else if(choice == 2){
-			gender = "female";
-		}
-		else {
-			std::cout<<"Invalid choice. Try again";
-		}
-	}while(choice!=1 && choice!=2);
-	
-	std::cout<<"ID no: ";
-	std::cin>>id_no;
-	std::cout<<"Select User type"<<std::endl;
-	std::cout<<"1. Student"<<std::endl;
-	std::cout<<"2. Staff"<<std::endl;
-	std::cout<<"3. Admin"<<std::endl;
-	std::cout<<"Your choice: "<<std::endl;
-	std::cin>>choice;
-	do{
-		if(choice == 1){
-			is_admin = 0;
-			is_staff = 0;
-		}
-		else if(choice == 2){
-			is_admin = 0;
-			is_staff = 1;
-		}
-		else if(choice == 3){
-			is_admin = 1;
-			is_staff = 1;
-		}
-		else {
-			std::cout<<"Invalid choice. Try again";
-		}
-	}while(choice!=1 && choice!=2 && choice!=3);
-	std::cout<<"Enter Default password"<<std::endl;
-	std::cin>>password;
+void User::add_user(char n[200],std::string g,char p[15],char id[20], bool stf, bool admn, char pass[255]){
+	strcpy(name,n);
+	gender = g;
+	strcpy(phone_number,p);
+	strcpy(id_no,id);
+	is_staff = stf;
+	is_admin = admn;
 }
 
 void Calendar::print_header(const int year, const int month) {
@@ -240,35 +195,10 @@ void Calendar::print_calendar(int year, int month) {
 int main(int argc, char** argv) {
 	Book book;
 	User user;
-	int choice {0};
-	std::cout<<"\tCounty Library management system"<<std::endl;
-	std::cout<<"\nWelcome, What would you like to do today: \n";
-	std::cout<<"1. Add User\n";
-	std::cout<<"2. Add Book\n";
-	std::cout<<"3. Print Calendar Month\n";
-	std::cout<<"Your choice: ";
-	std::cin>>choice;
-	switch(choice) {
-		case 1:
-			user.add_user();
-			std::cout<<"User Successfully added"<<std::endl;
-			break;
-		case 2:
-			book.add_book();
-			std::cout<<"Book Successfully added"<<std::endl;
-			break;
-		case 3:
-			int year, month;
-			Calendar calendar;
-			std::cout<<"Enter year followed by month: ";
-			std::cin>>year;
-			std::cin>>month;
-			calendar.print_calendar(year,month);
-			break;
-		default:
-			std::cout<<"Invalid Choice\n";
-
-	}
+	Calendar calendar;
+	book.add_book("Grasp","Sanjay sarma","61234678",5);
+	user.add_user("Titus Njiru","male","0707015033","123456",true,false,"kimanthi");
+	calendar.print_calendar(2021,2);
 	
 	return 0;
 }
